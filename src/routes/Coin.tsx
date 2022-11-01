eed0b20367be9d458417525ba48f10432a6de1b7
An Yonghee <ayh0429@gmail.com>
2022-11-01 21:19:37 +0900

    react-dom-router set up

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,16 @@
+import { useParams } from "react-router";
+
+interface RouteParams {
+  coinId: string;
+}
+
+function Coin() {
+  const { coinId } = useParams<keyof RouteParams>() as RouteParams;
+
+  return (
+    <div>
+      <h1>Coin: {coinId}</h1>
+    </div>
+  );
+}
+export default Coin;
